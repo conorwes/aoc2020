9d54efdca13750e4aa7eec09bb0ff85a15eb11eb
conor.westphal <conor.westphal@ai-solutions.com>
2020-12-03 11:33:31 -0500

    day one

---
+++ AoC2020/Solutions/DayOne.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+// Handy function which accepts a vector of ints of any size and sums members
+int sumN(const std::vector<int>& values)
+{
+   int sum;
+   for(auto i : values)
+      sum += i;
+
+   return sum;
+}
+
+// Part One: Find the pair which sums to 2020 and return their product.
+int partOne(const std::vector<int>& vec) {
+   int sum;
+   for (size_t i = 0; i < vec.size(); i++) {
+      for (size_t j = i + 1; j < vec.size(); j++) {
+         sum = sumN({vec.at(i), vec.at(j)});
+         if (sum == 2020)
+            return vec.at(i) * vec.at(j);
+      }
+   }
+
+   return 1;
+}
+
+int partTwo(const std::vector<int>& vec) {
+   int sum;
+   for (size_t i = 0; i < vec.size(); i++) {
+      for(size_t j = 0; j < vec.size(); j++) {
+         for (size_t k = 0; k < vec.size(); k++) {
+            sum = sumN({vec.at(i), vec.at(j), vec.at(k)});
+            if (sum == 2020)
+               return vec.at(i) * vec.at(j) * vec.at(k);
+         }
+      }
+   }
+
+   return 1;
+}
+
+
+int main()
+{
+   std::ifstream in("../Inputs/DayOne.txt");
+   std::string buf;
+   std::vector<int> vec;
+   while (std::getline(in, buf)) {
+      vec.push_back(std::stoi(buf));
+   }
+
+   std::cout << "Part One: " << partOne(vec) << std::endl;
+   std::cout << "Part Two: " << partTwo(vec) << std::endl;
+
+   return 0;
+}
