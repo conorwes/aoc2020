5d3e171ac5c9ba288eb039954c9d6768c1675638
conor.westphal <conor.westphal@ai-solutions.com>
2020-12-09 16:41:30 -0500

    day 7 c++, starting day 8 no idea if it will be possible to do day 7 in ff...maybe using structs?

--- AoC2020/C++ Solutions/Day_07.cpp
+++ AoC2020/C++ Solutions/Day_07.cpp
@@ -3,169 +3,88 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <deque>
+#include <unordered_set>
+#include <regex> // after wasting several hours trying to get a parser working, I'm going to go with regex
 
-class Bag {
-public:
-    Bag(std::string&& type) { description = type;
-    }
+// Define types for convenience:
+using contained_t = std::unordered_map<std::string, std::unordered_set<std::string>>; // Type for bags which contain this bag
+using contains_t = std::unordered_map<std::string, std::unordered_map<std::string, int>>; // Type for bags which are contained by this bag
 
-    std::string getDescription() const {
-        return description;
-    }
-
-    void addChildBag(int count, const Bag& bag) {
-        childBags.emplace_back(std::make_pair(count, bag));
-    }
-
-    std::vector<std::pair<int, Bag>> getChildBags() const {
-        return childBags;
-    }
-
-    bool containsBagType(std::string&& bagType) const {
-        for(auto& cb : childBags) {
-            if (cb.second.getDescription() == bagType || 
-                cb.second.containsBagType(std::move(bagType))) {
-                return true;
-            }
-        }
-    }
-
-    void reportContents() const {
-        std::cout << getDescription() << ": ";
-        for (auto& cb : childBags) {
-            std::cout << cb.second.getDescription() << "{ ";
-            for (auto& cbb : cb.second.getChildBags()) {
-                std::cout << cbb.second.getDescription() << " ";
-            }
-            std::cout << "} ";
-        }
-
-        std::cout << std::endl << std::endl;
-    }
-
-private:
-
-    std::string description;
-    std::vector<std::pair<int, Bag>> childBags;
-};
-
-std::pair<std::string, std::vector<std::pair<int, std::string>>> parseRule(std::string&& line) {
+// Build contained and contains using regex
+void parseRules(std::vector<std::string> rules, contained_t& contained, contains_t& contains) {
+    // Credit for these regex patterns goes to https://github.com/Kazhuu:
+    std::regex parentPattern(R"((\w+ \w+) bags contain)");
+    std::regex childPattern(R"(,? (\d+) (\w+ \w+) bags?)");
     
-    auto cleanDescription = [](std::string&& description) {
-        return description.erase(description.find("bag"), std::string::npos);
-    };
-    
-    std::string parentBag;
-    std::vector<std::pair<int, std::string>> childBags;
-    int count = 0;
-    std::string description;
+    for (auto&r : rules) {
+        std::smatch parentMatch;
+        std::regex_search(r, parentMatch, parentPattern);
 
-    std::string delimiter = " contain ";
-    int length = line.find(delimiter);
-    parentBag = cleanDescription(std::move(line.substr(0, length)));
-    line.erase(0, length + delimiter.size());
+        contains.emplace(parentMatch[1], std::unordered_map<std::string, int>());
 
-    // this is a _mess_. do this much more cleanly
-    delimiter = ", ";
-    if (line.find(',') != std::string::npos)
-    { 
-        while (length != std::string::npos) {
-            length = line.find(" ");
-            count = std::stoi(line.substr(0, length));
-            line.erase(0, length + 1);
-            length = line.find(delimiter);
-            description = cleanDescription(std::move(line.substr(0, length)));
-            line.erase(0, length + 2);
-            length = line.find(delimiter);
+        auto start = std::sregex_iterator(r.begin(), r.end(), childPattern);
+        auto end = std::sregex_iterator();
 
-            childBags.emplace_back(std::make_pair(count, description));
-        }
+        for (std::sregex_iterator i = start; i != end; i++) {
+            std::smatch match = *i;
 
-        length = line.find(" ");
-        count = std::stoi(line.substr(0, length));
-        line.erase(0, length + 1);
-        length = line.find(".");
-        description = cleanDescription(std::move(line.substr(0, length)));
-        line.erase(0, length + 1);
+            contains[parentMatch[1]].emplace(match[2], std::stoi(match[1]));
 
-        childBags.emplace_back(std::make_pair(count, description));
-    }
-    else if (line.find("no other bags") != std::string::npos)
-    {
-        childBags.emplace_back(std::make_pair(0, "null"));
-    }
-    else {
-        length = line.find(" ");
-        count = std::stoi(line.substr(0, length));
-        line.erase(0, length + 1);
-        length = line.find(".");
-        description = cleanDescription(std::move(line.substr(0, length)));
-        line.erase(0, length + 1);
-
-        childBags.emplace_back(std::make_pair(count, description));
-    }
-
-    return std::make_pair(parentBag, childBags);
-}
-
-void fillBags(const std::pair<std::string, std::vector<std::pair<int, std::string>>>& rule, std::vector<Bag>& bagTypes) {
-    for (int i = 0; i < bagTypes.size(); i++) {
-        if (bagTypes.at(i).getDescription() == rule.first) {
-            for (auto& cb : rule.second) {
-                for (auto& b : bagTypes) {
-                    if (b.getDescription() == cb.second) {
-                        bagTypes.at(i).addChildBag(cb.first, b);
-                    }
-                }
+            if (!contained.emplace(match[2], std::unordered_set<std::string>({parentMatch[1]})).second) {
+                contained[match[2]].insert(parentMatch[1]);
             }
         }
     }
 }
 
-int partOne(const std::vector<std::pair<std::string, std::vector<std::pair<int, std::string>>>>& rules) {
-    std::vector<std::string> containerBags;
+int partOne(contained_t& input) {
+    std::deque<std::string> bag({"shiny gold"});
+    std::unordered_set<std::string> seen({"shiny gold"});
 
-    // first get lowest level: bags which explicitly state they can contain shiny gold bags
-    for (auto& r : rules) {
-        for (auto& c : r.second) {
-            if (c.second.find("shiny gold") != std::string::npos) {
-                containerBags.emplace_back(r.first);
+    while (!bag.empty()) {
+        std::string bagName = bag.front();
+        for (auto& b : input[bagName]) {
+            if (seen.insert(b).second) {
+                bag.push_back(b);
             }
         }
+
+        bag.pop_front();
     }
 
-    return containerBags.size();
+    return seen.size() - 1;
+}
+
+int countBags(contains_t& input, std::string bagName) {
+    int bagCount = 0;
+    for (auto& [bagName, count] : input[bagName]) {
+        bagCount += count + count * countBags(input, bagName);
+    }
+
+    return bagCount;
+}
+
+int partTwo(contains_t& input) {
+    return countBags(input, "shiny gold");
 }
 
 int main() {
     std::ifstream in("../Inputs/Day_07.txt");
     std::string line;
-    std::vector<std::pair<std::string, std::vector<std::pair<int, std::string>>>> rules; // gross
+    std::vector<std::string> rules;
 
     while (std::getline(in, line)) {
-        rules.emplace_back(parseRule(std::move(line)));
+        rules.emplace_back(line);
     }
 
-    std::vector<Bag> bagTypes;
-    for (auto& r : rules) {
-        bagTypes.emplace_back(std::move(r.first));
-    }
+    contained_t contained;
+    contains_t contains;
+    parseRules(rules, contained, contains);
 
-    //bagTypes[0].reportContents();
-
-    for (auto& r: rules) {
-        fillBags(r, bagTypes);
-    }
-
-    auto bag = bagTypes[0];
-    bag.reportContents();
-
-    /*for (auto& b: bagTypes) {
-        b.reportContents();
-    }*/
-
-    //std::cout << "Part One: " << partOne(rules) << std::endl;
-    //std::cout << "Part Two: " << partTwo(vec) << std::endl;
+    std::cout << "Part One: " << partOne(contained) << std::endl;
+    std::cout << "Part Two: " << partTwo(contains) << std::endl;
 
     return 0;
 }
