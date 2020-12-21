9a772ec06e4d62ffbeb97f1f1085fbd7eb6ebd1f
conor.westphal <conor.westphal@ai-solutions.com>
2020-12-21 14:40:53 -0500

    Revert "general refactor resulted in success:"
    This reverts commit 6dc0ac4b386106c3d232999c93fae1875abbbd30.
    
    committed the wrong file

--- AoC2020/C++ Solutions/Day_17.cpp
+++ AoC2020/C++ Solutions/Day_17.cpp
@@ -1,16 +1,19 @@
 #include "common.h"
 #include <algorithm>
 
-std::vector<std::string> input;
-uint64_t numCycles{6};
-uint64_t width, height, depth;
+using namespace std;
+using ll = long long;
 
-uint64_t countNeighbors(const std::vector<std::vector<std::vector<bool>>>& grid, const uint64_t& x, const uint64_t& y, const uint64_t& z) {
-    uint64_t count{0};
+vector<string> input;
+ll numCycles{6};
+ll width, height, depth, wDim;
 
-    for (uint64_t iz = std::max(0ull, z-1); iz < std::min(z+2, depth); iz++) {
-        for (uint64_t iy = std::max(0ull, y-1); iy < std::min(y+2, height); iy++) {
-            for (uint64_t ix = std::max(0ull, x-1); ix < std::min(x+2, width); ix++) {
+ll countNeighbors(const vector<vector<vector<bool>>>& grid, const ll& x, const ll& y, const ll& z) {
+    ll count{0};
+
+    for (ll iz = max(0ll, z - 1); iz < min(z + 2, depth); iz++) {
+        for (ll iy = max(0ll, y - 1); iy < min(y + 2, height); iy++) {
+            for (ll ix = max(0ll, x - 1); ix < min(x + 2, width); ix++) {
                 if (grid[iz][iy][ix]) {
                     count++;
                 }
@@ -24,30 +27,50 @@ uint64_t countNeighbors(const std::vector<std::vector<std::vector<bool>>>& grid,
     return count;
 }
 
-uint64_t partOne() {
+ll countNeighbors(const vector<vector<vector<vector<bool>>>>& grid, const ll& x, const ll& y, const ll& z, const ll& w) {
+    ll count{0};
+    for (ll iw = max(0ll, w - 1); iw < min(w + 2, wDim); iw++) {
+        for (ll iz = max(0ll, z - 1); iz < min(z + 2, depth); iz++) {
+            for (ll iy = max(0ll, y - 1); iy < min(y + 2, height); iy++) {
+                for (ll ix = max(0ll, x - 1); ix < min(x + 2, width); ix++) {
+                    if (grid[iw][iz][iy][ix]) {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    if (grid[w][z][y][x]) {
+        count--;
+    }
+    return count;    
+}
+
+ll partOne() {
     width = (2 * numCycles) + input[0].size();
     height = (2 * numCycles) + input.size();
     depth = (2 * numCycles) + 1;
 
-    std::vector<std::vector<std::vector<bool>>> grid (
-        depth, std::vector<std::vector<bool>> (
-            height, std::vector<bool> (
+    vector<vector<vector<bool>>> grid (
+        depth, vector<vector<bool>> (
+            height, vector<bool> (
                 width, false )));
 
-    for (uint64_t y = 0; y < input.size(); y++) {
-        for (uint64_t x = 0; x < input[y].size(); x++) {
+    for (ll y = 0; y < input.size(); y++) {
+        for (ll x = 0; x < input[y].size(); x++) {
             grid[numCycles][numCycles + y][numCycles + x] = input[y][x] == '#';
         }
     }
 
-    uint64_t lastCount;
-    for (uint64_t c = 0; c < numCycles; c++) {
+    ll lastCount;
+    for (ll c = 0; c < numCycles; c++) {
         lastCount = 0;
         auto newGrid = grid;
-        for (uint64_t z = 0; z < depth; z++) {
-            for (uint64_t y = 0; y < height; y++) {
-                for (uint64_t x = 0; x < width; x++) {
-                    uint64_t neighbors = countNeighbors(grid, x, y, z);
+        for (ll z = 0; z < depth; z++) {
+            for (ll y = 0; y < height; y++) {
+                for (ll x = 0; x < width; x++) {
+                    ll neighbors = countNeighbors(grid, x, y, z);
                     if (grid[z][y][x]) {
                         newGrid[z][y][x] = (neighbors == 2) || (neighbors == 3);
                     } else {
@@ -61,21 +84,66 @@ uint64_t partOne() {
             }
         }
 
-        grid = std::move(newGrid);
+        grid = move(newGrid);
+    }
+
+    return lastCount;
+}
+
+ll partTwo() {
+    width = (2 * numCycles) + input[0].size();
+    height = (2 * numCycles) + input.size();
+    depth = (2 * numCycles) + 1;
+    wDim = (2 * numCycles) + 1;
+
+    vector<vector<vector<vector<bool>>>> grid (
+        wDim, vector<vector<vector<bool>>> (
+            depth, vector<vector<bool>> (
+                height, vector<bool> (
+                    width, false ))));
+
+    for (ll y = 0; y < input.size(); y++) {
+        for (ll x = 0; x < input[y].size(); x++) {
+            grid[numCycles][numCycles][numCycles + y][numCycles + x] = input[y][x] == '#';
+        }
+    }
+
+    ll lastCount;
+    for (ll c = 0; c < numCycles; c++) {
+        lastCount = 0;
+        auto newGrid = grid;
+        for (ll w = 0; w < wDim; w++) {
+            for (ll z = 0; z < depth; z++) {
+                for (ll y = 0; y < height; y++) {
+                    for (ll x = 0; x < width; x++) {
+                        ll neighbors = countNeighbors(grid, x, y, z, w);
+                        if (grid[w][z][y][x]) {
+                            newGrid[w][z][y][x] = (neighbors == 2) || (neighbors == 3);
+                        } else {
+                            newGrid[w][z][y][x] = (neighbors == 3);
+                        }
+                        if (newGrid[w][z][y][x]) {
+                            lastCount++;
+                        }
+                    }
+                }
+            }
+        }
+        grid = move(newGrid);
     }
 
     return lastCount;
 }
 
 int main() {
-    std::ifstream in("../Inputs/Day_17.txt");
-    std::string worker;
-    while(std::getline(in, worker)) {
+    ifstream in("../Inputs/Day_17.txt");
+    string worker;
+    while(getline(in, worker)) {
         input.emplace_back(worker);
     }
 
-    std::cout << "Part One: " << partOne() << std::endl;
-    //std::cout << "Part Two: " << partTwo() << std::endl;
+    cout << "Part One: " << partOne() << endl;
+    cout << "Part Two: " << partTwo() << endl;
 
     return 0;
 }
