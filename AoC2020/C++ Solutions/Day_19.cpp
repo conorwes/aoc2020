311301d103a503061330582484b89773c892e2e9
conor.westphal <conor.westphal@ai-solutions.com>
2020-12-28 10:52:04 -0500

    day 19. after many failed attempts, I ultimately decided to learn from https://github.com/janpipek, so this is not my own solution

--- AoC2020/C++ Solutions/Day_19.cpp
+++ AoC2020/C++ Solutions/Day_19.cpp
@@ -1,50 +1,206 @@
+#ifndef _GLIBCXX_REGEX_STATE_LIMIT
+#define _GLIBCXX_REGEX_STATE_LIMIT 100000000
+#endif
+
 #include "common.h"
-#include <unordered_map>
+#include <algorithm>
+#include <sstream>
+#include <regex>
+#include <map>
+#include <memory>
 
 using namespace std;
-using ll = long long;
 
-vector<string> input;
-unordered_map<int, vector<vector<int>>> rules; // we'll treat char as int
+const int recursionLevel = 32;
 
-int main() {
-    ifstream in("../Inputs/Day_19.txt");
-    string worker;
-    vector<vector<int>> values;
-    while(getline(in, worker)) {
-        if(worker == "") {
-            continue;
-        } else if (!isalpha(worker[0])) {
-            auto tokens = split(trim(worker), ':');
-            
-            // need to evaluate rules to determine what we've got
-            if (tokens[1].find('|') != string::npos) {
-                //vector of vector<int>
-                size_t pipePos = tokens[1].find('|');
-                values.resize(2);
-                for(auto& i : split(trim(tokens[1].substr(0, pipePos - 1)), ' ')) {
-                    values[0].emplace_back(stoi(i));
-                }
+struct Rule {
+    virtual string pattern() const = 0;
 
-                for (auto& i : split(trim(tokens[1].substr(pipePos + 1)), ' ')) {
-                    values[1].emplace_back(stoi(i));
-                }
-            } else if (tokens[1].find("\"") != string::npos) {
-                // char
-                values.resize(1);
-                values[0][0] = (trim(tokens[1])[1]);
-            } else {
-                values.resize(1);
-                for(auto& i : split(trim(tokens[1]), ' ')) {
-                    values[0].emplace_back(stoi(i));
-                    std::cout << stoi(i) << " ";
-                }
-            }
-            rules[stoi(tokens[0])] = values;
-        } else {
-            input.emplace_back(worker);
-        }
+    bool checkMatch(const string& s) const {
+        regex reg(pattern());
+        return regex_match(s, reg);
+    }
+};
+
+class Literal : public Rule {
+public:
+    Literal(char c) : _character(c) {}
+
+    string pattern() const override {
+        return string(1, _character);
     }
 
+private:
+    const char _character;
+};
+
+class Choice : public Rule {
+public:
+    Choice(const vector<shared_ptr<Rule>>& rules) : _rules(rules) {}
+
+    string pattern() const override {
+        ostringstream oss;
+        oss << "(?:";
+        for (size_t i= 0; i < _rules.size(); i++) {
+            oss << _rules[i]->pattern();
+            if (i < _rules.size() - 1) {
+                oss << "|";
+            }
+        }
+        oss << ")";
+        return oss.str();
+    }
+
+private:
+    vector<shared_ptr<Rule>> _rules;
+};
+
+class Sequence : public Rule {
+public:
+    Sequence(const vector<shared_ptr<Rule>>& rules) : _rules(rules) {}
+
+    string pattern() const override {
+        ostringstream oss;
+        oss << "(?:";
+        for (const auto& rule : _rules) {
+            oss << rule->pattern();
+        }
+
+        oss << ")";
+        return oss.str();
+    }
+
+private:
+    vector<shared_ptr<Rule>> _rules;
+};
+
+class Proxy : public Rule {
+public:
+    Proxy(const shared_ptr<map<size_t, shared_ptr<Rule>>>& collection, size_t index) : _collection(collection),
+                                                                                       _index(index) {}
+
+    string pattern() const override {
+        auto realRule = _collection->at(_index);
+        return realRule->pattern();
+    }
+
+    size_t index() const {
+        return _index;
+    }
+
+private:
+    size_t _index;
+    const shared_ptr<map<size_t, shared_ptr<Rule>>> _collection;
+};
+
+struct Input {
+    const shared_ptr<map<size_t, shared_ptr<Rule>>> rules = make_shared<map<size_t, shared_ptr<Rule>>>();
+    vector<string> lines;
+};
+
+pair<size_t, shared_ptr<Rule>> parseRule(const string &line, shared_ptr<map<size_t, shared_ptr<Rule>>> ruleCollection) {
+    auto index_pattern = split(line, ": ");
+
+    size_t index = stoul(index_pattern[0]);
+    string pattern = index_pattern[1];
+
+    shared_ptr<Rule> rule;
+
+    auto parseSequence = [ruleCollection](const string &s) -> shared_ptr<Rule> {
+        auto subpatterns = split(s, ' ');
+        vector<shared_ptr<Rule>> sequence;
+        transform(subpatterns.cbegin(), subpatterns.cend(), back_inserter(sequence), [ruleCollection](auto &s) {
+            return make_shared<Proxy>(ruleCollection, stoul(s));
+        });
+        return make_shared<Sequence>(sequence);
+    };
+
+    if (pattern[0] == '"') {
+        rule = make_shared<Literal>(pattern[1]);
+    } else if (pattern.find('|') != string::npos) {
+        auto subpatterns = split(pattern, " | ");
+        vector<shared_ptr<Rule>> choice;
+        transform(subpatterns.cbegin(), subpatterns.cend(), back_inserter(choice), parseSequence);
+        rule = make_shared<Choice>(choice);
+    } else {
+        rule = parseSequence(pattern);
+    }
+
+    return {index, rule};
+}
+
+Input readInput() {
+    auto sections = readSections("../Inputs/Day_19.txt");
+    Input result{};
+    for (const auto &line: sections[0]) {
+        auto parsedRule = parseRule(line, result.rules);
+        result.rules->insert({parsedRule.first, parsedRule.second});
+    }
+    result.lines = sections[1];
+    return result;
+}
+
+class Rule8 : public Rule {
+public:
+    Rule8(const shared_ptr<map<size_t, shared_ptr<Rule>>> &collection) : _collection(collection) {}
+
+    string pattern() const override {
+        auto realRule = _collection->at(42);
+        return "(?:" + realRule->pattern() + "+" + ")";
+    }
+
+private:
+    const shared_ptr<map<size_t, shared_ptr<Rule>>> _collection;
+};
+
+class Rule11 : public Rule {
+public:
+    Rule11(const shared_ptr<map<size_t, shared_ptr<Rule>>> &collection) : _collection(collection) {}
+
+    string pattern() const override {
+        const string p42 = _collection->at(42)->pattern();
+        const string p31 = _collection->at(31)->pattern();
+
+        ostringstream oss;
+        oss << "(";
+
+        for (int i = 1; i < recursionLevel; i++) {
+            ostringstream curly;
+            curly << "{" << i << "}";
+
+            ostringstream exact;
+            oss << "(" << p42 << curly.str() << p31 << curly.str() << ")";
+
+            if (i < recursionLevel - 1) {
+                oss << "|";
+            }
+        }
+        oss << ")";
+        return oss.str();
+    }
+
+private:
+    const shared_ptr<map<size_t, shared_ptr<Rule>>> _collection;
+};
+
+int partOne() {
+    auto input = readInput();
+    auto rule0 = input.rules->at(0);
+    return count_if(input.lines.cbegin(), input.lines.cend(), [rule0](auto line) { return rule0->checkMatch(line); });
+}
+
+int partTwo() {
+    auto input = readInput();
+    auto rule0 = input.rules->at(0);
+
+    (*input.rules)[8] = make_shared<Rule8>(input.rules);
+    (*input.rules)[11] = make_shared<Rule11>(input.rules);
+
+    return count_if(input.lines.cbegin(), input.lines.cend(), [rule0](auto line) { return rule0->checkMatch(line); });
+}
+
+int main() {
+    std::cout << "Part One: " << partOne() << std::endl;
+    std::cout << "Part Two: " << partTwo() << std::endl;
     return 0;
 }
--- AoC2020/C++ Solutions/common.h
+++ AoC2020/C++ Solutions/common.h
@@ -16,6 +16,30 @@ inline std::vector<std::string> split(std::string str, char delimiter) {
     return tokens;
 }
 
+std::vector<std::string> split(const std::string &original, const std::string &delimiter, int maxSplits = -1)
+{
+	std::vector<std::string> result;
+	int splits = 0;
+	size_t offset = 0;
+
+	while (true)
+	{
+		if (splits == maxSplits)
+		{
+			break;
+		}
+		size_t pos = original.find(delimiter, offset);
+		if (pos == std::string::npos)
+		{
+			break;
+		}
+		result.push_back(original.substr(offset, pos - offset));
+		offset = pos + delimiter.length();
+	}
+	result.push_back(original.substr(offset));
+	return result;
+}
+
 inline std::string trim(std::string str) {
     size_t beginPos = str.find_first_not_of(" \t");
     if (beginPos == std::string::npos) {
@@ -26,4 +50,33 @@ inline std::string trim(std::string str) {
     size_t len = endPos - beginPos + 1;
 
     return str.substr(beginPos, len);
+}
+
+std::vector<std::string> readLines(const std::string &basename)
+{
+	std::ifstream in(basename);
+	std::string buffer;
+	std::vector<std::string> lines;
+	while (getline(in, buffer))
+	{
+		lines.push_back(buffer);
+	}
+	return lines;
+}
+
+std::vector<std::vector<std::string>> readSections(const std::string &basename) {
+    auto lines = readLines(basename);
+    std::vector<std::vector<std::string>> result;
+    std::vector<std::string> currentSection;
+    for (const auto& line: lines) {
+        if (line.empty()) {
+            result.push_back(currentSection);
+            currentSection = {};
+        }
+        else {
+            currentSection.push_back(line);
+        }
+    }
+    result.push_back(currentSection);
+    return result;
 }
